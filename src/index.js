ebd7a3cafb3b0c1647c7c46ecf09fbfa3e41aca6
dorasampleproj <kattaanaveen@gmail.com>
2025-12-02 09:21:32 +0000

    test 2 of build failure

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log("Starting deployment test...");
-
-// Simulate failure
-process.exit(1);  // 0 = success, anything else = failure
-
 root.render(
   <React.StrictMode>
     <App />
