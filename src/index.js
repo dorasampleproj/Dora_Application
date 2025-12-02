40d8a6d0d2a32fa93728b26aa11be83d4369158c
dorasampleproj <kattaanaveen@gmail.com>
2025-12-02 09:09:23 +0000

    test commit to fail build

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "@/index.css";
 import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log("Starting deployment test...");
+
+// Simulate failure
+process.exit(1);  // 0 = success, anything else = failure
+
 root.render(
   <React.StrictMode>
     <App />
